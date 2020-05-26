3358ba05f8b9dee3905384e83fa3f10b0fa56f3a
Vincent-Chung-SpecialTopic <10836025@ntub.edu.tw>
2020-05-27 00:58:09 +0800

    服務管理頁面初步建置之對話盒加入

--- MyRehabilitaion/app/src/main/java/com/example/myrehabilitaion/FragmentHomePage/Statistics.java
+++ MyRehabilitaion/app/src/main/java/com/example/myrehabilitaion/FragmentHomePage/Statistics.java
@@ -1,5 +1,6 @@
 package com.example.myrehabilitaion.FragmentHomePage;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
@@ -11,63 +12,36 @@ import android.view.ViewGroup;
 
 import com.example.myrehabilitaion.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Statistics#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Statistics extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+/*
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendForm;
+import com.github.mikephil.charting.components.Legend.LegendPosition;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ValueFormatter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+ */
+//版权声明：本文为CSDN博主「zhangphil」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+//原文链接：https://blog.csdn.net/zhangphil/java/article/details/47656521
+
+public class Statistics extends Fragment {
+
 
     public Statistics() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Statistics.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Statistics newInstance(String param1, String param2) {
-        Statistics fragment = new Statistics();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_statistics, container, false);
-
-        return v;
+        View root =  inflater.inflate(R.layout.fragment_statistics, container, false);
 
 
 
+        return root;
     }
+
 }
