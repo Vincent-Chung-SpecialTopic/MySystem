58818b6f587255e23dac6a5af0b1392549b47a24
Vincent-Chung-SpecialTopic <10836025@ntub.edu.tw>
2020-05-30 23:36:15 +0800

    UI大新增

--- MyRehabilitaion/app/src/main/java/com/example/myrehabilitaion/InforCard.java
+++ MyRehabilitaion/app/src/main/java/com/example/myrehabilitaion/InforCard.java
@@ -3,12 +3,20 @@ package com.example.myrehabilitaion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class FunctionPage extends AppCompatActivity {
+public class InforCard extends AppCompatActivity {
+    ImageButton imgbtnback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_function_page);
+        setContentView(R.layout.activity_infor_card);
+
+
+
+
     }
 }
